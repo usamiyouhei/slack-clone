780407bc32a3679075985a88308e82fb84444beb
youhei usami <165367050+youhei-usami@users.noreply.github.com>
2025-06-15 09:31:20 +0400

    new

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import CreateWorkspace from './pages/CreateWorkspace'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+   <BrowserRouter>
+   <div>
+    <Routes>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/" element={<CreateWorkspace />}/>
+      <Route path="/:workspaceId/:channelId" element={<Home />}/>
+    </Routes>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
